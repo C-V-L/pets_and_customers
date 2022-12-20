39b03dee18ed1bb1a6ed564822cc6aa722abb905
Conner Van Loan <cdvanloan@gmail.com>
2022-12-20 08:12:49 -0800

    Adds adopt method to customer

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -8,6 +8,17 @@ RSpec.describe Customer do
 
   it 'exisits and has attributes' do
     expect(@joel).to be_a(Customer)
+    expect(@joel.name).to eq("Joel")
+    expect(@joel.id).to eq(2)
+  end
+
+  it 'starts without pets and can adopt' do
+    expect(@joel.pets).to eq([])
+    samson = Pet.new({name: "Samson", type: :dog, age: 3})
+    lucy = Pet.new({name: "Lucy", type: :cat, age: 12})
+    @joel.adopt(samson)
+    @joel.adopt(lucy)
+    expect(@joel.pets).to eq([samson, lucy])
   end
 
 end
