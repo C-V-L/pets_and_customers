a10b2a2745b461a845de1e4a18c71e72ed20bbe7
Conner Van Loan <cdvanloan@gmail.com>
2022-12-21 15:12:19 -0800

    Minor edits to fix spacing

--- spec/vet_clinic_spec.rb
+++ spec/vet_clinic_spec.rb
@@ -15,8 +15,6 @@ RSpec.describe VetClinic do
     @simba = Pet.new({name: "Simba", type: :cat, age: 6})
     @kimmy.adopt(@spot)
     @kimmy.adopt(@simba)
-    @vet_clinic.add_customer(@joel)
-    @vet_clinic.add_customer(@kimmy)
   end
 
   it 'exisits and has a name' do
@@ -28,6 +26,11 @@ RSpec.describe VetClinic do
     expect(@vet_clinic.customers).to include(@joel, @kimmy)
   end
 
+  before(:each) do
+    @vet_clinic.add_customer(@joel)
+    @vet_clinic.add_customer(@kimmy)
+  end
+  
   it 'can list pets sorted by age' do
     expect(@vet_clinic.pets_at_vet_clinic).to eq([@lucy, @simba, @samson, @spot])
   end
@@ -38,6 +41,5 @@ RSpec.describe VetClinic do
     @frankenkitty = Pet.new({name: "Franky", type: :cat, age: 6})
     @kimmy.adopt(@frankenkitty)
     expect(@vet_clinic.number_of_pets(@kimmy)).to eq(3)
-
   end
 end
